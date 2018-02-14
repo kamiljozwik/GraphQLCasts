75ee5cf88d9cdb9b962ca9b9690a440972a98ebb
kamiljozwik <kamil.jozwik@o2.pl>
2018-02-14 20:28:12 +0100

    Update server.js

--- users/server.js
+++ users/server.js
@@ -4,9 +4,9 @@ const schema = require('./schema/schema');
 
 const app = express();
 
-app.use('/graphql', expressGraphQL({
-  schema,
-  graphiql: true
+app.use('/graphql', expressGraphQL({ // mówimy aplikacji, żeby słuchała tylko zapytań dla GraphQL (czyli na path localhost:4000/graphql)
+  schema,  // schema są to wszystkie relacje w naszej bazie danych (nasz graph)
+  graphiql: true  // deeloper tool that allows making query to our developer server. Tylko używamy w środowisku developerskim
 }));
 
 app.listen(4000, () => {
