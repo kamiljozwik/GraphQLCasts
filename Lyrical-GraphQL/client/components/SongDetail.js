75b464342ce0f393da36883d6444ad937c6c3fd9
kamiljozwik <kamil.jozwik@o2.pl>
2018-03-03 19:25:02 +0100

    Update SongDetail.js

--- Lyrical-GraphQL/client/components/SongDetail.js
+++ Lyrical-GraphQL/client/components/SongDetail.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { graphql } from 'react-apollo';
+import { graphql } from 'react-apollo';           // helper
 import { Link } from 'react-router';
 import fetchSong from '../queries/fetchSong';
 import LyricCreate from './LyricCreate';
@@ -9,19 +9,19 @@ class SongDetail extends Component {
   render() {
     const { song } = this.props.data;
 
-    if (!song) { return <div>Loading...</div>; }
+    if (!song) { return <div>Loading...</div>; }                // czekamy na wykonanie query
 
     return (
       <div>
         <Link to="/">Back</Link>
         <h3>{song.title}</h3>
-        <LyricList lyrics={song.lyrics} />
-        <LyricCreate songId={this.props.params.id} />
+        <LyricList lyrics={song.lyrics} />                      // song.lyrics = podajemy dalej do komponentu kolekcję składającą się z wierszy tekstu
+        <LyricCreate songId={this.props.params.id} />           // podajemy dalej songId, ponieważ ReactRouter podaje "params" tylko do pierwszego spotkanego komponentu
       </div>
     );
   }
 }
 
-export default graphql(fetchSong, {
-  options: (props) => { return { variables: { id: props.params.id } } }
+export default graphql(fetchSong, {         // drugim agumentem jest obiekt. Dzięki temu podajemy do query parametry (zmienne). W naszym przypadku jest to id piosenki którą chemy pobrać. ID jest podane w path Routera
+  options: (props) => { return { variables: { id: props.params.id } } } //graphql helper bierze propsy komponentu (tu: "(props)") i sam podaje je dalej do query. To co zwrócone przez return trafia do query jako query variables
 })(SongDetail);
