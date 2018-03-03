2e589ccb77c86b6764c82401dfb5e36e98697a51
kamiljozwik <kamil.jozwik@o2.pl>
2018-03-03 19:24:55 +0100

    Update LyricList.js

--- Lyrical-GraphQL/client/components/LyricList.js
+++ Lyrical-GraphQL/client/components/LyricList.js
@@ -3,29 +3,29 @@ import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
 class LyricList extends Component {
-  onLike(id, likes) {
-    this.props.mutate({
+  onLike(id, likes) {             // przekazujemy likes, aby zaimplementować Optymistic Updates
+    this.props.mutate({           // wywołanie mutation do like'owania linii wersów
       variables: { id },
-      optimisticResponse: {
-        __typename: 'Mutation',
-        likeLyric: {
+      optimisticResponse: {       // implementacji Optimistic Updates - przewidywanie odpowiedzi z serwera i błyskawiczne obrazowanie jej na froncie zanim odpowiedź fizycznie do nas dotrze
+        __typename: 'Mutation',   // zawsze dodajemy tą linię
+        likeLyric: {              // dokładna odpowiedź jaką oczekujemy mieć z serwera backendowego; można w consoli sobie podejrzeć jak wygląda (w chrome zakładka Network -> XHR)
           id,
           __typename: 'LyricType',
-          likes: likes + 1
+          likes: likes + 1        // przewidujemy, że like doda się poprawnie
         }
       }
     });
   }
 
   renderLyrics() {
-    return this.props.lyrics.map(({ id, content, likes }) => {
+    return this.props.lyrics.map(({ id, content, likes }) => {     // id and content and likes quantity of every single lyric (wiersz piosenki)
       return (
         <li key={id} className="collection-item">
           {content}
           <div className="vote-box">
             <i
               className="material-icons"
-              onClick={() => this.onLike(id, likes)}
+              onClick={() => this.onLike(id, likes)}  // użyta arrow function, aby mieć dostęp do "id:
             >
               thumb_up
             </i>
@@ -45,7 +45,7 @@ class LyricList extends Component {
   }
 }
 
-const mutation = gql`
+const mutation = gql`             // mutation do like'owania pojedynczych wersów
   mutation LikeLyric($id: ID) {
     likeLyric(id: $id) {
       id
